5a19bb0cf516861aaed3e8ddaaeec234e255dbbf
Marco Bazzoni <bazzoni.marco@gmail.com>
2015-05-24 11:37:18 +0200

    Added plugin type

--- cms-plugin-iface/src/main/java/sparkle/cms/plugin/mgmt/PluginType.java
+++ cms-plugin-iface/src/main/java/sparkle/cms/plugin/mgmt/PluginType.java
@@ -1,9 +1,42 @@
 package sparkle.cms.plugin.mgmt;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * PluginType
  * Created by bazzoni on 24/05/2015.
  */
 public enum PluginType {
-    ASSET_MGMT
+    ASSET_MGMT("ASSET_MGMT");
+
+    public static final PluginType[] ALL = {ASSET_MGMT};
+
+    private final String name;
+
+    PluginType(final String name) {
+        this.name = name;
+    }
+
+    @JsonCreator
+    public static PluginType forName(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null for type");
+        }
+        if (name.toUpperCase().equals("ASSET_MGMT")) {
+            return ASSET_MGMT;
+        }
+        throw new IllegalArgumentException("Name \"" + name + "\" does not correspond to any Feature");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return getName();
+    }
+
 }
